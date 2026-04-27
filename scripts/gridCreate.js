dc1914980d8adb0e4e228e8fcd44e4c2297cb36a
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-04-27 06:47:59 -0400

    Add: Color divs on mouseover

--- scripts/gridCreate.js
+++ scripts/gridCreate.js
@@ -1,19 +1,3 @@
- /*
-    Create a grid of 16 x 16 on the webpage within a div.
-
-    FUNCTION createGrid
-        CONST finalNumber EQUALS 16
-        LET count EQUALS 0
-        
-        WHILE count LESS THAN OR EQUAL finalNumber
-            let division EQUALS DOCUMENT CREATEELEMENT div
-            gridArea.APPENDCHILD division 
-            count PLUS EQUAL 1
-
-LET gridArea EQUALS DOCUMENT QUERYSELECTOR .grid-area  
-
-    To be continued... Have to implement grid first.
- */
 
 function createGrid() {
     const finalCount = 255;
@@ -21,12 +5,6 @@ function createGrid() {
 
     while(count <= finalCount){
         let division = document.createElement('div');
-        division.textContent = "";
-        /*
-        division.addEventListener('mouseenter', () => {
-            division.style.backgroundColor = 'orange';
-        })
-        */
         gridArea.appendChild(division);
         count += 1;
     }
@@ -35,14 +13,8 @@ function createGrid() {
         if(event.target.tagName === 'DIV'){
             event.target.style.backgroundColor = 'orange';
             console.log('entered div');
-        } 
-    });
-
-    gridArea.addEventListener('mouseout', event => {
-        if(event.target.tagName === 'DIV'){
-            event.target.style.backgroundColor = "";
         }
-    })
+    });
 
 }
 
