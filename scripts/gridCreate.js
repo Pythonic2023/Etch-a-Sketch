83164a32c6557664b31eacdab7c93c49e55e225c
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-04-29 12:54:49 -0400

    Add prompt to add number of squares wanted.

--- scripts/gridCreate.js
+++ scripts/gridCreate.js
@@ -2,20 +2,38 @@ let gridSizePrompt = document.querySelector('.grid-size-prompt');
 gridSizePrompt.addEventListener('click', removeGrid);
 
 function removeGrid(event){
+    let number = Number(prompt("Number: "));
     const divisionToRemove = document.querySelectorAll('div');
     let remove = divisionToRemove.forEach(item => {
         gridArea.removeChild(item);
-    })
+    });
+    if(number > 100){
+        alert('Must not exceed 100');
+        createGrid(Number(16));
+    }else {
+        createGrid(number);
+    }
 }
 
-function createGrid() {
-    let finalCount = 255;
-    let count = 0;
+function createGrid(number) {
+    if(number){
+        count = 0;
+        calculateFinalNumber = number * number;
 
-    while(count <= finalCount){
-        let division = document.createElement('div');
-        gridArea.appendChild(division);
-        count += 1;
+        while(count <= calculateFinalNumber){
+            let division = document.createElement('div');
+            gridArea.appendChild(division);
+            count += 1;
+        }
+    } else{
+        let finalCount = 255;
+        let count = 0;
+
+        while(count <= finalCount){
+            let division = document.createElement('div');
+            gridArea.appendChild(division);
+            count += 1;
+            }
     }
 
 
